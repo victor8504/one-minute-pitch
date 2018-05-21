705e3d0f71117f793ecf0c1acb7dd5c085910ef6
victor njuguna <victormoore254@gmail.com>
2018-05-21 14:47:08 +0300

    create the logout route

--- app/auth/views.py
+++ app/auth/views.py
@@ -29,3 +29,9 @@ def login():
 
     title = "watchlist login"
     return render_template('auth/login.html',login_form = login_form, title = title)
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("main.index"))
