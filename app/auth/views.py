eebb13e65b6418c2afc44b6ddda5d83d29b38ba6
victor njuguna <victormoore254@gmail.com>
2018-05-21 14:29:14 +0300

    create the registration html

--- app/auth/views.py
+++ app/auth/views.py
@@ -2,7 +2,7 @@ from flask import render_template,redirect,url_for, flash,request
 from . import auth
 from flask_login import login_user,logout_user,login_required
 from ..models import User
-from .forms import RegistrationForm,LoginForm
+from .forms import RegistrationForm
 from .. import db
 
 @auth.route('/register',methods = ["GET","POST"])
@@ -12,8 +12,6 @@ def register():
         user = User(email = form.email.data, username = form.username.data,password = form.password.data)
         db.session.add(user)
         db.session.commit()
-
-        mail_message("Welcome to watchlist","email/welcome_user",user.email,user=user)
 
         return redirect(url_for('auth.login'))
         title = "New Account"
