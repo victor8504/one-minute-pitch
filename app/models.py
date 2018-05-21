1656d6b0ee76bc795f300eeccfd7257998add153
victor njuguna <victormoore254@gmail.com>
2018-05-21 16:05:43 +0300

    create a class category and its columns and incoporated it into the database

--- app/models.py
+++ app/models.py
@@ -7,12 +7,40 @@ from . import login_manager
 def load_user(user_id):
     return User.query.get(int(user_id))
 
+class Category(db.Model):
+    '''
+    Category class define category per pitch
+    '''
+    __tablename__ = 'categories'
+
+    # add columns
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(255))
+    description = db.Column(db.String(255))
+
+    # save
+    def save_category(self):
+        '''
+        Function that saves a category
+        '''
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_categories(cls):
+        '''
+        Function that returns all the data from the categories after being queried
+        '''
+        categories = Category.query.all()
+        return categories
+
 class User(UserMixin,db.Model):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255),index = True)
     email = db.Column(db.String(255),unique = True,index = True)
+
     bio = db.Column(db.String(255))
     profile_pic_path = db.Column(db.String())
 
